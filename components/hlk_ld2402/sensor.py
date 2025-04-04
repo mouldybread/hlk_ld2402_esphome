1d8eddb353987504a6b529da50160741800abc89
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-04-05 05:14:30 +0800

    Fix

--- components/hlk_ld2402/sensor.py
+++ components/hlk_ld2402/sensor.py
@@ -14,23 +14,17 @@ DEPENDENCIES = ["hlk_ld2402"]
 # Create a dedicated sensor class for distance
 HlkLd2402DistanceSensor = hlk_ld2402_ns.class_("HlkLd2402DistanceSensor", sensor.Sensor)
 
-# Define a sensor platform in your component
-CONF_DISTANCE = "distance"
-
-# Register the component as a platform of sensor
-CONFIG_SCHEMA = cv.Schema({
+# Make this a proper platform
+CONFIG_SCHEMA = sensor.sensor_schema(
+    HlkLd2402DistanceSensor,
+    device_class=DEVICE_CLASS_DISTANCE,
+    state_class=STATE_CLASS_MEASUREMENT,
+    unit_of_measurement=UNIT_CENTIMETER,
+).extend({
     cv.Required(CONF_HLK_LD2402_ID): cv.use_id(HLKLD2402Component),
-    cv.Optional(CONF_DISTANCE): sensor.sensor_schema(
-        device_class=DEVICE_CLASS_DISTANCE,
-        state_class=STATE_CLASS_MEASUREMENT,
-        unit_of_measurement=UNIT_CENTIMETER,
-    ),
 })
 
 async def to_code(config):
+    var = await sensor.new_sensor(config)
     parent = await cg.get_variable(config[CONF_HLK_LD2402_ID])
-    
-    if CONF_DISTANCE in config:
-        var = cg.new_Pvariable(config[CONF_DISTANCE][CONF_ID])
-        await sensor.register_sensor(var, config[CONF_DISTANCE])
-        cg.add(parent.set_distance_sensor(var))
+    cg.add(parent.set_distance_sensor(var))
